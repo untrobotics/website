db847e80785d4e3aceba985731d4e05dea217ef2
Sebastian King <sebastian.king@btinternet.com>
2019-01-25 20:34:50 +0100

    improved endpoint documentation and structure

---
+++ api/endpoint-example/groupme-endpoint-config.php
@@ -0,0 +1,6 @@
+<?php
+define('SECRET_CODE', 'GaqyJvB5a3XsjGXADrKkzgwT'); // I recommend using: https://www.random.org/passwords/?num=1&len=24&format=html&rnd=new
+define('ACCESS_TOKEN', 'R3Cbjn3ygjrDmPPnU3ofbHWWgVxwTio4A3BYAjJ2');
+define('CHANNEL_ID', '45617130');
+define('API_URL', 'https://api.groupme.com/v3/groups/' . $channel_id . '/messages?token=' . $access_token);
+?>
