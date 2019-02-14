47283e3a4db8033a204596a5068656715b20c4f9
Sebastian King <sebastian-king@users.noreply.github.com>
2019-02-14 15:35:58 -0600

    Update hackdfw.php

--- hackdfw.php
+++ hackdfw.php
@@ -11,49 +11,56 @@
 </ul>
 
 Team 2:
+<ul>
      <li>Chris Panko</li>
      <li>Joe Niu</li>
      <li>Israel Makambu</li>
      <li>Answesh Baisyat</li>
      <li>Keyon Geason</li>
-
+</ul>
 Team 3:
+<ul>
      <li>Seb King</li>
      <li>Nick Tindle</li>
      <li>Jesse Sainz</li>
      <li>Ibrahim Al-Kuwaifi</li>
      <li>Kimberlee Ashmore</li>
-
+</ul>
 Team 4:
+<ul>
      <li>Danilo Claros</li>
      <li>Clayton Clouser</li>
      <li>Christy Nguyen</li>
      <li>Jonathon Wright</li>
      <li>Uchenna Ajiwe</li>
-
+</ul>
 Team 5:
+<ul>
      <li>Casey Kinnamon</li>
      <li>Natalie Drewfs</li>
      <li>Alex Bacallao</li>
      <li>Brendan Nyambati</li>
      <li>Skylar Werner</li>
-
+</ul>
 Team 6:
+<ul>
      <li>Ben Taylor</li>
      <li>Rhythm Greg</li>
      <li>(Other non UNT members)</li>
-
+</ul>
 Team 7:
+<ul>
      <li>Zach Harte</li>
      <li>(Other non UNT members)</li>
-
+</ul>
 Team 8:
+<ul>
      <li>Raghib Aftab</li>
      <li>Bassam Metwally</li>
      <li>Mohammed Alsaadi</li>
      <li>Rickie Barroso</li>
      <li>Thong "Dan" Nguyen</li>
-
+</ul>
 
 <h2>Carpool Details</h2>
 
@@ -61,26 +68,31 @@ Team 8:
 <p>The carpool groups are</p>
 
 Car 1:
+<ul>
      <li>Mohammed Alsaadi (Driver)</li>
      <li>Ibrahim Al-Kuwaifi</li>
      <li>Joe Niu</li>
      <li>Jonathon Wright</li>
-
+</ul>
 Car 2:
+<ul>
      <li>Bassam Metwally (Driver)</li>
      <li>Danilo Claros</li>
      <li>Clayton Clouser</li>
      <li>Raghib Aftab</li>
      <li>Christy Nguyen</li>
-
+</ul>
 Car 3:
+<ul>
      <li>Sebastian King (Driver)</li>
      <li>Nicholas Tindle</li>
      <li>Natalie Drewfs</li>
      <li>Chris Panko</li>
-
+</ul>
 Car 4:
+<ul>
      <li>Israel Makambu (Driver)</li>
      <li>Keyon Geason</li>
      <li>Aleandro "Alex" Baca</li>
      <li>Skylar Werner</li>
+</ul>
