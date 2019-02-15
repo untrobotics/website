005b022041067729ab83190f82e134a0336d33cc
Sebastian King <sebastian-king@users.noreply.github.com>
2019-02-14 21:52:45 -0600

    Update hackdfw.php

--- hackdfw.php
+++ hackdfw.php
@@ -65,7 +65,13 @@
      </li>
 </ul>
 
-<h2 id="event-info">3. More Info</h2>
+<h2 id="event-info">3. Event Info</h2>
+
+<h4>Directions:</h4>
+<p><a href="https://goo.gl/maps/QUAs6Gfu6QP2">Women's Museum in Fair Park,<br>3800 Parry Ave.,<br>Dallas, Tx<br>75226</a></p>
+<p>You can park in any open space (no permit required) in Fair Park, entering through Gate 3. See the map below for more instructions.</p>
+<div><img src="/images/hackdfw/map.png/></div>
+
 <h4><a href="https://hackdfw.com/guidebook.pdf">Hack DFW Event Guidebook</a></h4>
 
 <h2 id="teams">4. Teams</h2>
