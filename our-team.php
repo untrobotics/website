c551596ca98253fe923bc101dee75d8bc598c755
Sebastian King <sebastian.king@btinternet.com>
2020-01-29 00:48:47 +0100

    added our-team

---
+++ our-team.php
@@ -0,0 +1,182 @@
+<?php
+require('template/top.php');
+head('Our Team', true);
+?>
+<main class="page-content">
+        <!-- Classic Breadcrumbs-->
+        <section class="breadcrumb-classic">
+          <div class="rd-parallax">
+            <div data-speed="0.25" data-type="media" data-url="images/breadcrumbs-parallax.jpg" class="rd-parallax-layer"></div>
+            <div data-speed="0" data-type="html" class="rd-parallax-layer section-top-75 section-md-top-150 section-lg-top-260">
+              <div class="shell">
+                <ul class="list-breadcrumb">
+                  <li><a href="/">Home</a></li>
+                  <li><a href="/about">About Us</a></li>
+                  <li>Our Team</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="section-75 section-md-100 section-lg-150">
+          <div class="shell range-offset-1">
+            <div class="range">
+              <div class="cell-lg-6">
+                <h1>Our Team</h1>
+                <h6>These people are the reason for our success and expertise.</h6>
+              </div>
+            </div>
+            <div class="range">
+              <div class="cell-lg-6 cell-sm-6 cell-md-12">
+                <div class="range range-sm-middle">
+                  <div class="cell-md-6"><img src="images/bio-pics/nick-tindle.jpg" alt="" width="360" height="404" class="img-responsive"/>
+                  </div>
+                  <div class="cell-md-6">
+                    <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
+                      <h6 class="h6-with-small"><a href="#"> Nicholas Tindle</a><span class="small text-silver-chalice">President</span></h6>
+                      <p>I am a junior, studying Computer Engineering. I work on projects for the university and go to hackathons in his free time. I also likes robots a bit.</p>
+                      <ul class="list-inline-lg">
+                        <li><a href="https://www.linkedin.com/in/nicholas-tindle-658886144" class="icon icon-sm text-primary fa-linkedin"></a></li>
+                        <li><a href="https://www.twitter.com/@nicktindle" class="icon icon-sm text-primary fa-twitter"></a></li>
+                        <li><a href="https://www.github.com/ntindle" class="icon icon-sm text-primary fa-github"></a></li>
+			<li><a href="mailto:president@untrobotics.com" class="icon icon-sm text-primary fa-envelope"></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="cell-lg-6 cell-sm-6 cell-md-12 offset-top-60 offset-lg-top-0 offset-sm-top-0 offset-md-top-60">
+                <div class="range range-sm-middle">
+                  <div class="cell-md-6"><img src="images/bio-pics/juan-ruiz.jpg" alt="" width="360" height="404" class="img-responsive"/>
+                  </div>
+                  <div class="cell-md-6">
+                    <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
+                      <h6 class="h6-with-small"><a href="#"> Juan Ruiz</a><span class="small text-silver-chalice">Vice President</span></h6>
+                      <p>I am a Computer Engineering Student, I.T. Support Technician, and Undergrad Research Assistant at The University of North Texas. I am an active member of The Alpha Tau Omega Fraternity, Society of Hispanic Professional Engineers, Engineers without Borders, IEEE, and National Society of Professional Engineers. I have also interned at NASA, U.S. Department of Energy, and iOLAP. My focus is Embedded Systems but I have a strong passion for robotics, automation, machine learning, and artificial intelligence.</p>
+                      <ul class="list-inline-lg">
+                        <li><a href="#" class="icon icon-sm text-primary fa-facebook"></a></li>
+                        <li><a href="#" class="icon icon-sm text-primary fa-twitter"></a></li>
+                        <li><a href="#" class="icon icon-sm text-primary fa-instagram"></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="range">
+              <div class="cell-lg-6 cell-sm-6 cell-md-12">
+                <div class="range range-sm-middle">
+                  <div class="cell-md-6"><img src="images/bio-pics/chase-summers.jpg" alt="" width="360" height="404" class="img-responsive"/>
+                  </div>
+                  <div class="cell-md-6">
+                    <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
+                      <h6 class="h6-with-small"><a href="#"> Chase Summers</a><span class="small text-silver-chalice">Secretary</span></h6>
+                      <p>Hello! My names Chase, I'm a transfer student from Weatherford College located in (you guessed it) Weatherford! I'm currently a Junior pursuing a Bachelors degree in Information Technology (IT). I found my passion for IT from my current job in Geek Squad. At the beginning of my Academic Career I was dead set on becoming a detective, I thought that would be the coolest job ever. But I soon realized how depressing it would be working with dead bodies all day. Eventually a position opened up for Geek Squad at my local Best Buy, I applied and realized that IT and being a detective are kinda similar (hear me out). So when someone comes in with an issue with a computer I have to use deductive reasoning to decide on what the issue is and if it can be repaired in store or out of store.</p>
+                      <ul class="list-inline-lg">
+                        <li><a href="#" class="icon icon-sm text-primary fa-facebook"></a></li>
+                        <li><a href="#" class="icon icon-sm text-primary fa-twitter"></a></li>
+                        <li><a href="#" class="icon icon-sm text-primary fa-instagram"></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="cell-lg-6 cell-sm-6 cell-md-12 offset-top-60 offset-lg-top-0 offset-sm-top-0 offset-md-top-60">
+                <div class="range range-sm-middle">
+                  <div class="cell-md-6"><img src="images/bio-pics/tim-stern.jpg" alt="" width="360" height="404" class="img-responsive"/>
+                  </div>
+                  <div class="cell-md-6">
+                    <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
+                      <h6 class="h6-with-small"><a href="#"> Tim Stern</a><span class="small text-silver-chalice">Treasurer</span></h6>
+                      <p>I am a Junior, studying Computer Engineering with a minor in Mathematics</p>
+                      <ul class="list-inline-lg">
+                        <li><a href="#" class="icon icon-sm text-primary fa-facebook"></a></li>
+                        <li><a href="#" class="icon icon-sm text-primary fa-twitter"></a></li>
+                        <li><a href="#" class="icon icon-sm text-primary fa-instagram"></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="range">
+              <div class="cell-lg-6 cell-sm-6 cell-md-12">
+                <div class="range range-sm-middle">
+                  <div class="cell-md-6"><img src="images/bio-pics/eric-king.jpg" alt="" width="360" height="404" class="img-responsive"/>
+                  </div>
+                  <div class="cell-md-6">
+                    <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
+                      <h6 class="h6-with-small"><a href="#"> Eric King</a><span class="small text-silver-chalice">Inventory Manager</span></h6>
+                      <p>I am a 4th year Electrical Engineering student here at UNT. I have a strong interest in robotics and automation. My goal here at UNT Robotics is to create a place where fellow students can apply their knowledge from the classroom and have the opportunity to learn new skills.</p>
+                      <ul class="list-inline-lg">
+                        <li><a href="#" class="icon icon-sm text-primary fa-facebook"></a></li>
+                        <li><a href="#" class="icon icon-sm text-primary fa-twitter"></a></li>
+                        <li><a href="#" class="icon icon-sm text-primary fa-instagram"></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="cell-lg-6 cell-sm-6 cell-md-12 offset-top-60 offset-lg-top-0 offset-sm-top-0 offset-md-top-60">
+                <div class="range range-sm-middle">
+                  <div class="cell-md-6"><img src="images/bio-pics/david-woodward.jpg" alt="" width="360" height="404" class="img-responsive"/>
+                  </div>
+                  <div class="cell-md-6">
+                    <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
+                      <h6 class="h6-with-small"><a href="#"> David Woodward</a><span class="small text-silver-chalice">Event Coordinator</span></h6>
+                      <p>I am an Undergraduate student at the University of North Texas studying Computer Engineering. I have interests in embedded systems, computer vision, robotics, cryptocurrency, drones, and audio. In my spare time I enjoy playing guitar, producing music, working on projects, and gaming.</p>
+                      <ul class="list-inline-lg">
+                        <li><a href="#" class="icon icon-sm text-primary fa-facebook"></a></li>
+                        <li><a href="#" class="icon icon-sm text-primary fa-twitter"></a></li>
+                        <li><a href="#" class="icon icon-sm text-primary fa-instagram"></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="range">
+              <div class="cell-lg-6 cell-sm-6 cell-md-12">
+                <div class="range range-sm-middle">
+                  <div class="cell-md-6"><img src="images/bio-pics/kyle-derrough.jpg" alt="" width="360" height="404" class="img-responsive"/>
+                  </div>
+                  <div class="cell-md-6">
+                    <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
+                      <h6 class="h6-with-small"><a href="#"> Kyle Derrough</a><span class="small text-silver-chalice">Public Relations</span></h6>
+                      <p>I study Electrical Engineering Technology, my goal as part of UNT Robotics is to continue to learn and push the envelope to ensure that the work that we are doing is recognized for the opportunities we create and outstanding projects that we see through.</p>
+                      <ul class="list-inline-lg">
+                        <li><a href="#" class="icon icon-sm text-primary fa-facebook"></a></li>
+                        <li><a href="#" class="icon icon-sm text-primary fa-twitter"></a></li>
+                        <li><a href="#" class="icon icon-sm text-primary fa-instagram"></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="cell-lg-6 cell-sm-6 cell-md-12 offset-top-60 offset-lg-top-0 offset-sm-top-0 offset-md-top-60">
+                <div class="range range-sm-middle">
+                  <div class="cell-md-6"><img src="images/bio-pics/sebastian-king.jpg" alt="" width="360" height="404" class="img-responsive"/>
+                  </div>
+                  <div class="cell-md-6">
+                    <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
+                      <h6 class="h6-with-small"><a href="#"> Sebastian King</a><span class="small text-silver-chalice">Webmaster</span></h6>
+		      <p>I am the Webmaster for UNT Robotics. I'm in charge of the website that you see in front of you. I also integrate our technology stack with other organisations and maintain the online equipment checkout system.</p>
+                      <p>I'm almost finished studying Computer Science and Electrical Engineering, so robotics is the natural choice for all of my talents. I love to be hands on with technology, and take apart and break anything that I can. For me there's nothing quite like finally seeing a project successfully working out in the real-world.
+</p>
+                      <ul class="list-inline-lg">
+			<li><a href="https://www.linkedin.com/in/sebastian-king" class="icon icon-sm text-primary fa-linkedin"></a></li>
+                        <li><a href="https://www.twitter.com/@thekingseb" class="icon icon-sm text-primary fa-twitter"></a></li>
+                        <li><a href="https://www.github.com/sebastian-king" class="icon icon-sm text-primary fa-github"></a></li>
+			<li><a href="mailto:webmaster@untrobotics.com" class="icon icon-sm text-primary fa-envelope"></a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+<?php
+footer();
+?>
