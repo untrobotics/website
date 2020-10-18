191f5fc365c093156b85f1c169387357f52e8b2a
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2020-10-18 08:58:31 +0200

    Add alumni page, move about links to a new section

---
+++ about/our-alumni.php
@@ -0,0 +1,120 @@
+<?php
+require('../template/top.php');
+head('Our Alumni', true);
+?>
+    <style>
+        .bio-area {
+            border-top: 2px solid #ececec;
+            margin-top: 10px;
+            padding-top: 10px;
+        }
+    </style>
+    <main class="page-content">
+        <!-- Classic Breadcrumbs-->
+        <section class="breadcrumb-classic">
+            <div class="rd-parallax">
+                <div data-speed="0.25" data-type="media" data-url="/images/breadcrumbs-parallax.jpg" class="rd-parallax-layer"></div>
+                <div data-speed="0" data-type="html" class="rd-parallax-layer section-top-75 section-md-top-150 section-lg-top-260">
+                    <div class="shell">
+                        <ul class="list-breadcrumb">
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/about">About Us</a></li>
+                            <li>Our Alumni</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-75 section-md-100 section-lg-150">
+            <div class="shell range-offset-1">
+                <div class="range">
+                    <div class="cell-lg-6">
+                        <h1>Our Alumni</h1>
+                        <h6>These people are the ones responsible for building our organisation and getting us to where we are today.</h6>
+                    </div>
+                </div>
+                <!-- 1st President -->
+                <div class="cell-lg-12 bio-area">
+                    <div class="range range-sm-middle">
+                        <div class="cell-md-3"><img src="/images/bio-pics/nick-tindle.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        </div>
+                        <div class="cell-md-6">
+                            <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
+                                <h6 class="h6-with-small"><a href="#"> Nick Tindle</a><span class="small text-silver-chalice">1st President</span></h6>
+                                <p>I am a Computer Engineering Student, The first president, and by far a better president than Juan. (FACTS!)</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- 2nd President -->
+                <div class="cell-lg-12 bio-area">
+                    <div class="range range-sm-middle">
+                        <div class="cell-md-3"><img src="/images/bio-pics/juan-ruiz.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        </div>
+                        <div class="cell-md-6">
+                            <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
+                                <h6 class="h6-with-small"><a href="#"> Juan Ruiz</a><span class="small text-silver-chalice">2nd President</span></h6>
+                                <p>I am a Computer Engineering Student, I.T. Support Technician, and Undergrad Research Assistant at The University of North Texas. I am an active member of The Alpha Tau Omega Fraternity, Society of Hispanic Professional Engineers, Engineers without Borders, IEEE, and National Society of Professional Engineers. I have also interned at NASA, U.S. Department of Energy, and iOLAP. My focus is Embedded Systems but I have a strong passion for robotics, automation, machine learning, and artificial intelligence.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Former Secretary -->
+                <div class="cell-lg-12 bio-area">
+                    <div class="range range-sm-middle">
+                        <div class="cell-md-3"><img src="/images/bio-pics/katie-lee.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        </div>
+                        <div class="cell-md-6">
+                            <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
+                                <h6 class="h6-with-small"><a href="#"> Katie Lee</a><span class="small text-silver-chalice">Former Secretary</span></h6>
+                                <p>I’m a transfer student from UH. I’m studying Computer Engineering because I want to be on the front of the newest technology development, and I’m the secretary for UNT Robotics.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Former Event Coordinator -->
+                <div class="cell-lg-12 bio-area">
+                    <div class="range range-sm-middle">
+                        <div class="cell-md-3"><img src="/images/bio-pics/michelle-vargas.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        </div>
+                        <div class="cell-md-6">
+                            <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
+                                <h6 class="h6-with-small"><a href="#"> Michelle Rosal Vargas</a><span class="small text-silver-chalice">Former Event Coordinator</span></h6>
+                                <p>Mechanical Engineer | Event Coordinator at UNT Robotics</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Former Public Relations -->
+                <div class="cell-lg-12 bio-area">
+                    <div class="range range-sm-middle">
+                        <div class="cell-md-3"><img src="/images/bio-pics/andrew-jarrett.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        </div>
+                        <div class="cell-md-6">
+                            <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
+                                <h6 class="h6-with-small"><a href="#"> Andy Jarrett</a><span class="small text-silver-chalice">Former Public Relations</span></h6>
+                                <p>Mechanical Engineer | Public Relations at UNT Robotics</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Former Project Manager -->
+                <div class="cell-lg-12 bio-area">
+                    <div class="range range-sm-middle">
+                        <div class="cell-md-3"><img src="/images/bio-pics/nicole-kohm.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        </div>
+                        <div class="cell-md-6">
+                            <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
+                                <h6 class="h6-with-small"><a href="#">Nicole Kohm</a><span class="small text-silver-chalice">Former Project Manager</span></h6>
+                                <p>I am an adaptive problem-solver with a lifetime of scientific passion. I enjoy applying what I learn to make new ideas and possibilities come to fruition. I have a strong track record of careful attention to detail and thinking outside the box. I frequently invent new technologies and methods to quickly solve multidisciplinary problems.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php
+footer();
+?>
