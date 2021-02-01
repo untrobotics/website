c39d1cfb6fa8b6d1971b1e1bad743ecc72e5a1a8
peyton <peyton.t131@gmail.com>
2021-01-31 19:23:22 -0600

    updated botathon page

--- botathon/index.php
+++ botathon/index.php
@@ -13,6 +13,11 @@ head('Botathon Info', true);
 		font-family: Consolas, "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", Monaco, "Courier New", "monospace";
 		color: black;
 	}
+
+    #partsTable1{
+        width: 70%;
+    }
+
 	.parts tr {
 		border-bottom: 1px solid gray;
 	}
@@ -20,14 +25,14 @@ head('Botathon Info', true);
 		color: black;
 	}
 	.parts tr td:first-of-type {
-		width: 200px;
+		width: 150px;
 		font-weight: 800;
 	}
 	.parts tr td:nth-of-type(2) {
 		width: 150px;
 	}
 	.parts.basekit tr td:nth-of-type(3) {
-		width: 200px;
+		width: 250px;
 	}
 	.parts.upgrades tr td:nth-of-type(3) {
 		width: 150px;
@@ -57,7 +62,7 @@ head('Botathon Info', true);
 	}
 	.sponsors .sponsor {
 		display: inline-block;
-		margin: 0px 20px;
+		margin: 0 20px;
 		padding: 10px;
 	}
 	
@@ -117,7 +122,7 @@ head('Botathon Info', true);
                   <h4 class="text-regular">Navigation</h4>
                   <ul class="list-xs list-marked">
                     <li class="active"><a href="#info" class="text-content">What is Botathon</a></li>
-					<li><a href="#spnsors" class="text-content">Event Sponsors</a></li>
+					<li><a href="#sponsors" class="text-content">Event Sponsors</a></li>
                     <li><a href="#rules" class="text-content">Rules</a></li>
                     <li><a href="#schedule" class="text-content">Schedule</a></li>
                     <li><a href="#field-preview" class="text-content">Field Preview</a></li>
@@ -139,23 +144,25 @@ head('Botathon Info', true);
             <div class="range range-md-justify">
               <div class="cell-md-12 cell-lg-10">
                 <div class="inset-md-right-30 inset-lg-right-0">
-					<h1><strong>Botathon</strong> - <em>Season 1</em></h1>
-					<p>Botathon is UNT’s first ever hosted robotics competition, think hackathon+robots!</p>
+					<h1><strong>Botathon</strong> - <em>Season 2</em></h1>
+					<p>Botathon is UNT’s second ever hosted robotics competition, think hackathon+robots!</p>
                   
-                  	<p>Participants will have 8 hours to build a robot then compete head to head in a race to pop the most balloons in our custom built arena. This event is open to all UNT students, with any amount of experience.</p>
+                  	<p>Participants will have 3 weeks to build a robot then compete head to head in a race to collect the most treasure on our custom built island. This event is open to all UNT students, with any amount of experience.</p>
                   
                   	<div class="well-custom">
                     	<h5>Open to all UNT students, learn something new and make new friends.</h5>
                   	</div>
                   
-					<p>The goal of this event is to give students a chance to learn how to build a robot and compete against their peers. Botathon is free to all UNT students and will feature free food, workshops, tech demos, and prizes!</p>
+					<p>The goal of this event is to give students a chance to learn how to build a robot and compete against their peers. Botathon is free to all UNT students and will be completely virtual this year, with competitors building robots with a group virtually and controlling their robots remotely from home.</p>
 					
 					<p style="margin-top:40px; font-size: 22px;"><strong>When/Where is it happening?</strong></p>
-					<h4 style="margin-top: 10px;"><em>10am-10pm</em>, <strong>April 27th, 2019</strong></h4>
+                    <h4>February 1st : Registration Begins</h4>
+                    <h4>March 22 - April 7th : Build Period</h4>
+                    <h4>March 29 - April 7th: Inspection Period</h4>
+					<h4 style="margin-top: 30px;"><em>10am-3pm</em>, <strong>April 10th, 2021</strong></h4>
 					<h3>@ <strong>UNT Discovery Park</strong></h3>
 					
-					<p>Our official informational packet is available <a href="/downloads/botathon-info-packet.pdf">here</a>.</p>
-               
+					<p>Our official informational packet is available <a href="/downloads/botathon/botathon-info-packet.pdf">here</a>.</p>
                 </div>
               </div>
             </div>
@@ -168,8 +175,6 @@ head('Botathon Info', true);
             <div class="range">
 			 	<div class="cell-md-8 cell-lg-9">
                 	<div class="sponsors">
-						<div class="sponsor"><img src="/images/sponsor-logos/botathon/monster-energy.jpg"/></div>
-						<div class="sponsor"><img src="/images/sponsor-logos/botathon/marcos-pizza.jpg"/></div>
 						<div class="sponsor"><img src="/images/sponsor-logos/botathon/eagles-nest.jpg"/></div>
 						<div class="sponsor"><img src="/images/sponsor-logos/botathon/elegoo.jpg"/></div>
 					</div>
@@ -184,10 +189,11 @@ head('Botathon Info', true);
             <div class="range">
 			 	<div class="cell-md-8 cell-lg-9">
                 	<div class="inset-md-right-30 inset-lg-right-0">
-						<p>Teams of up to four students will be given two robot kits <em>(consult the parts list below to see what these contain)</em>, and 8 hours in which you may build as many robots as you want.</p>
-						<p><strong>The challenge:</strong> to use your team’s robots to pop the other team’s balloons, while defending your own.</p>					<p>Each side of the field contains 13 balloons, and each team must place 4 balloons on their robot(s). The objective is to pop as many of the opposite team’s balloons within 4 minutes, the team with the most points when the time runs out, wins.</p>
-						<p>Of the 13 balloons on each side, eight will be placed at ground level and five will be placed on the elevated ramps in the middle of the arena. There is one point awarded for each ground-level balloon popped, two points for each heightened balloon and three points for each balloon that is on a robot. If a team can pop all of 17 of the opponent's balloons, they earn a five-point bonus. This works out to a maximum of 35 points in a single round.</p>
-						<p>For information about contact and penalties, please consult the <a href="/downloads/botathon-info-packet.pdf">information packet</a>.</p>
+						<p>Teams of up to four students will be given a robot kit <em>(consult the parts list below to see what these contain)</em>, and 3 weeks in which they may build a robot ship. The robot ship will then be submitted for inspection where UNT Robotics will hold it until competition day.</p>
+                        <p><strong>The challenge: </strong>Collaborate with an opposing pirate crew to fire cannons and defeat The Kraken as quickly as possible, gaining access to The Treasure Room. From there it's an all out race against the opposing crew to collect the most treasure.</p>					<p>Each side of the field contains 13 balloons, and each team must place 4 balloons on their robot(s). The objective is to pop as many of the opposite team’s balloons within 4 minutes, the team with the most points when the time runs out, wins.</p>
+						<p>Stage 1 consists of The Kraken room. It contains The Kraken, and 3 elevated platforms with cannons on them. The objective is to work together with the opposing pirate crew to fire all 3 of the cannons, effectively disabling the Kraken and unlocking The Treasure Room. Pirate crews will be given the same score in this stage, which depends on the time taken to take down The Kraken.</p>
+						<p>The second stage consists of The Treasure Room. This room is randomly filled with an assortment of treasure, with the highest quality treasure being on raised platforms. The objective in this stage is to push treasure into your Coffer located on one side of the field. Points will be based on both the quantity and quality of treasure collected. </p>
+                        <p>For information about contact and penalties, please consult the <a href="/downloads/botathon/botathon-info-packet.pdf">information packet</a>.</p>
 					</div>
 				</div>
 			</div>
@@ -201,18 +207,12 @@ head('Botathon Info', true);
 			 	<div class="cell-md-8 cell-lg-9">
                 	<div class="inset-md-right-30 inset-lg-right-0">
 						<table id="schedule">
-							<tr><td>&nbsp;7:30 am</td><td>Volunteer Call time</td></tr>
-							<tr><td>&nbsp;9:00 am</td><td>Check-in begins</td></tr>
-							<tr><td>10:00 am</td><td>Competition Rules and Event Overview</td></tr>
-							<tr><td>10:30 am</td><td>Development/Part Check-Out period begins</td></tr>
-							<tr><td>10:30 am</td><td>Workshop: Crash Course in Robotics and Team Building</td></tr>
-							<tr><td>12:00 pm</td><td>Lunch</td></tr>
-							<tr><td>&nbsp;1:00 pm</td><td>Demo: IEEE R5 Autonomous robot demo and Q/A</td></tr>
-							<tr><td>&nbsp;3:00 pm</td><td>Resume / Career Workshop</td></tr>
-							<tr><td>&nbsp;6:00 pm</td><td>Dinner</td></tr>
-							<tr><td>&nbsp;6:30 pm</td><td>Competition Begins</td></tr>
-							<tr><td>&nbsp;9:00 pm</td><td>Finals</td></tr>
-							<tr><td>&nbsp;9:30 pm</td><td>Winners announced and prizes distributed</td></tr>
+							<tr><td>&nbsp;10:00 am</td><td>Volunteer Call Time</td></tr>
+                            <tr><td>&nbsp;10:30 am</td><td>Online Check-In & Review of Event and Rules</td></tr>
+							<tr><td>&nbsp;11:00 am</td><td>Competition Begins</td></tr>
+							<tr><td>&nbsp;12:30 pm</td><td>Lunch Break</td></tr>
+							<tr><td>&nbsp;1:00 pm</td><td>Resume Competition</td></tr>
+							<tr><td>&nbsp;3:00 pm</td><td>Winners Announced</td></tr>
 						</table>
 					</div>
 				</div>
@@ -223,6 +223,8 @@ head('Botathon Info', true);
 		<section class="section-50" id="field-preview">
 			<div class="shell">
 			  <h2>Field preview</h2>
+                <h3>TBA</h3>
+                <!--
 			  <div class="range">
 				  <div class="cell-md-8 cell-lg-9">
                 	<div class="inset-md-right-30 inset-lg-right-0 field-preview">
@@ -249,6 +251,7 @@ head('Botathon Info', true);
 					  </div>
 				  </div>
 			  </div>
+                -->
 			</div>
 		</section>
 		   
@@ -259,31 +262,49 @@ head('Botathon Info', true);
 			 	<div class="cell-md-8 cell-lg-9">
                 	<div class="inset-md-right-30 inset-lg-right-0">
 						
-						<h4>Base Kit</h4>
-						<p>Each team gets two base kits, which consist of the below items.</p>
-						
-						<table class="parts basekit">
+						<h4>Base Kits</h4>
+						<p>Each team will get to choose which base kit they want to build their pirate ship with.</p>
+
+                        <h4>OMNI WHEEL CAR ROBOT:</h4>
+						<table class="parts basekit" id = "partsTable1">
 							<tr><th>Part Name</th><th>Quantity</th></tr>
-							<tr><td>Motor</td><td>2</td></tr>
-							<tr><td>Wheels</td><td>2</td></tr>
-							<tr><td>Arduino</td><td>2</td></tr>
-							<tr><td>2WD Chassis</td><td>1</td></tr>
-							<tr><td>Caster Wheel</td><td>1</td></tr>
-							<tr><td>Battery Harness</td><td>1</td></tr>
-							<tr><td>Motor Controller</td><td>1</td></tr>
-							<tr><td>AA Battery</td><td>4</td></tr>
-							<tr><td>Motor</td><td>2</td></tr>
-							<tr><td>Power Switch</td><td>1</td></tr>
-							<tr><td>BLE Module</td><td>1</td></tr>
-							<tr><td>Motor</td><td>2</td></tr>
+							<tr><td>UNO R3 Development Board</td><td>1</td></tr>
+							<tr><td>L293D Drive Board</td><td>1</td></tr>
+							<tr><td>HC-05 Bluetooth Module</td><td>1</td></tr>
+							<tr><td>HC-SR04 Ultrasonic Module</td><td>1</td></tr>
+							<tr><td>Four-Way Tracking Module</td><td>1</td></tr>
+							<tr><td>Infrared Detection Module</td><td>2</td></tr>
+							<tr><td>DC Motor</td><td>4</td></tr>
+							<tr><td>Rubber Wheels </td><td>4</td></tr>
+							<tr><td>Servo</td><td>1</td></tr>
+							<tr><td>Speed Code Disk</td><td>1</td></tr>
+							<tr><td>Screws & Bolts</td><td>79</td></tr>
+							<tr><td>Wires</td><td>50</td></tr>
 						</table>
+
+                        <h4 style = "margin-top: 70px;">RUBBER WHEEL CAR ROBOT:</h4>
+                        <table class="parts basekit" id = "partsTable1">
+                            <tr><th>Part Name</th><th>Quantity</th></tr>
+                            <tr><td>CH340G UNO R3 Development Board</td><td>1</td></tr>
+                            <tr><td>UNO Expansion Board</td><td>1</td></tr>
+                            <tr><td>L298N Driver Board</td><td>1</td></tr>
+                            <tr><td>HC-SR04 Ultrasonic Module</td><td>1</td></tr>
+                            <tr><td>Ultrasonic Bracket</td><td>1</td></tr>
+                            <tr><td>18650 Battery Box</td><td>1</td></tr>
+                            <tr><td>M3*15 Double Pass Copper Pillar</td><td>4</td></tr>
+                            <tr><td>M3 Nut </td><td>2</td></tr>
+                            <tr><td>M3*8 Screw</td><td>10</td></tr>
+                            <tr><td>20cm Female to Female Dupont Wire</td><td>16</td></tr>
+                            <tr><td>Mecanum Wheel Single-Layer Chassis</td><td>1</td></tr>
+                            <tr><td>Mecanum Wheel Single-Layer Alloy Car</td><td>1</td></tr>
+                        </table>
 						
 						
 						<h4 style="margin-top: 40px;">Upgrades</h4>
-						<p>These parts are able to be bought (using in-game currency) and will be in addition to the base kits parts that you are given freely. Each team is given $500 of in-game currency.</p>
+						<p>These parts are able to be bought with doubloons (in-game currency) and will be in addition to the base kits parts that you are given freely. Each team is given 500 doubloons of in-game currency.</p>
 						<table class="parts upgrades">
 							<tr><th>Part Name</th><th>Quantity</th><th>Price (game currency)</th></tr>
-							<tr><td>Arduino</td><td>1</td><td>$150</td></tr>
+							<tr><td>Arduino</td><td>1</td><td>$50</td></tr>
 							<tr><td>Raspberry Pi</td><td>1</td><td>$300</td></tr>
 							<tr><td>Servo</td><td>1</td><td>$50</td></tr>
 							<tr><td>Metal Gear Servo</td><td>1</td><td>$100</td></tr>
@@ -313,8 +334,8 @@ head('Botathon Info', true);
             <div class="range">
 			 	<div class="cell-md-8 cell-lg-9">
                 	<div class="inset-md-right-30 inset-lg-right-0">
-						<p>Teams will consist of 4 members. Each team will be given 2 base kits and $500 of in-game currency to buy upgrade parts. Participants are encouraged to form their team prior to the event, but there will be team making opportunities on the day for individuals without a team.</p>
-						<p>There will be mentors to assist teams during the build process, but mentors will not be assigned to a specific team.</p>
+						<p>Pirate Crews will consist of 3-4 members. Each crew will be given a base kit and 500 doubloons of in-game currency to buy upgrade parts for their ship. Participants are encouraged to form their crew prior to registration, but single entry participants can be randomly matched with a pirate crew.</p>
+						<p>There will be mentors to assist crews during the build process in our Discord as well as virtual build sessions. </p>
 					</div>
 				</div>
 			</div>
