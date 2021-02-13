a34fe2c7945543a7103e61f9dd3e6bc0d16aa522
Sebastian King <sebastian.king@btinternet.com>
2021-02-13 20:07:27 +0100

    Add merch changes, add discord rules

---
+++ legal/discord-rules.php
@@ -0,0 +1,66 @@
+<?php
+require("../template/top.php");
+head("Discord Rules", true);
+?>
+<style>
+    h3 {
+        margin-top: 20px;
+    }
+</style>
+
+<section class="breadcrumb-classic">
+    <div class="shell">
+        <ul class="list-breadcrumb">
+            <li><a href="/">Home</a></li>
+            <li><a href="/legal">Legal</a></li>
+            <li>Discord Rules</li>
+        </ul>
+    </div>
+</section>
+
+<section class="section-50 section-md-75 section-lg-100">
+    <div class="shell text-sm-left">
+        <div class="range">
+            <div class="cell-xl-12">
+                <div>
+                    <h2>Discord Rules</h2>
+                    <p>The following set of rules govern communications in the official UNT Robotics Discord Guild/Server.</p>
+                    <h3>Access</h3>
+                        <ol>
+                            <li>The UNT Robotics Discord server is open to all current UNT students.</li>
+                            <li>UNT Robotics alumni are permitted to remain part of the server after their graduation.</li>
+                            <li>UNT Faculty are permitted to join and use the server.</li>
+                            <li>Any non-UNT students or faculty must be vetted before joining the server. All non-UNT affiliated members must first complete all appropriate paperwork with UNT to be allowed to work with students. The main criteria for non-UNT affiliated members are as follows:</li>
+                            <li>A professional working in the industry who is a mentor or volunteer.</li>
+                            <li>A member of an affiliated professional organization, such as IEEE, who is a mentor, volunteer, or associate.</li>
+                        </ol>
+                    <h3>Communications</h3>
+                        <ol>
+                            <li>Follow the UNT Student Code of Conduct. This includes the Academic Dishonesty policies, such as rules on sharing papers, code, and assignments. All violations will be reported to the UNT Dean of Students.</li>
+                            <li>Be nice. You will get muted or banned if you are rude to anyone.</li>
+                            <li>Do not spam, advertise, or solicit in any of our channels. Your post will be deleted, and you may be muted or banned.</li>
+                        </ol>
+                    <h3>Content</h3>
+                        <ol>
+                            <li>No explicit, unsafe or NSFW content is permitted in the channel. Posting any of these can lead to a ban from the server and a referral to the UNT Dean of Students.</li>
+                            <li>Offensive or rude memes will not be allowed. This includes memes containing imagery which may appear innocuous, but which have been designated as hate symbols by the ADL.</li>
+                        </ol>
+                    <h3>Names</h3>
+                        <ol>
+                            <li>
+                                <p>Your name in the Discord server must reflect your full real name, which will be verified with UNT Directory Services to validate your identity.</p>
+                                <p><em>Why is this necessary?</em><br />This is necessary because UNT Robotics is an official UNT Student Organisation and therefore we have a duty of care to our students. To ensure the safety of those in the Discord we must be able to verify all participants' identities, and make sure they are UNT students and faculty. Furthermore, UNT admits students who are minors and therefore extra precautions must be taken to ensure the safety and privacy of minors in our organization. We are duty bound to help enforce the UNT Code of Conduct, and we cannot do so without knowing who each person in the server is such that if any violations occur, we must be able to verify who committed the violation.</p>
+                            </li>
+                        </ol>
+                    <h3>Future Rules</h3>
+                    <ol>
+                        <li>If we must add a rule because of you, you are likely going to get banned.</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+footer();
+?>
