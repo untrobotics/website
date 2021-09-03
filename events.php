e639b6319b92000f106760e45c3c7e34b121148f
Henry <henlegay@gmail.com>
2021-09-02 19:34:37 -0500

    This fixed the resizing issue on mobile for the calendar

--- events.php
+++ events.php
@@ -2,21 +2,28 @@
 require('template/top.php');
 head('Events Calendar', true);
 ?>
+    <style>
+        .calendarFix{
+            border: 0;
+            width: 80vw;
+            height: 80vw;
+        }
 
-<main class="page-content">
+    </style>
+    <main class="page-content">
         <section class="section-25 section-md-50 section-lg-75">
-			<h1 class="text-center">Events Calendar</h1>
-			<div class="text-center">For the most up-to-date information please <a href="/discord">join our Discord</a>.</div>
-			<br>
-                <div class="shell text-sm-left">
-                        <div class="range text-center">
-                                <div class="col-lg-12">
-					<iframe src="https://calendar.google.com/calendar/embed?src=untroboticsclub%40gmail.com&ctz=America%2FChicago" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-                                </div>
-                        </div>
+            <h1 class="text-center">Events Calendar</h1>
+            <div class="text-center">For the most up-to-date information please <a href="/discord">join our Discord</a>.</div>
+            <br>
+            <div class="shell text-sm-left">
+                <div class="range text-center">
+                    <div class="col-lg-12">
+                        <iframe src="https://calendar.google.com/calendar/embed?src=untroboticsclub%40gmail.com&ctz=America%2FChicago" class="calendarFix" style="border: 0" height="600" frameborder="0" scrolling="no"></iframe>
+                    </div>
                 </div>
+            </div>
         </section>
-</main>
+    </main>
 
 <?php
 footer();
