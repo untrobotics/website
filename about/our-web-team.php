3d414442e0774a0a5c76d562fa2733c38d44c9d7
shotbyapony <masonbesmer@gmail.com>
2021-09-27 23:42:59 -0500

    fixed everything, coolness

--- about/our-web-team.php
+++ about/our-web-team.php
@@ -29,18 +29,18 @@ head('Our Team', true);
             <div class="shell range-offset-1">
                 <div class="range">
                     <div class="cell-lg-6">
-                        <h1>Our Web Team</h1>
+                        <h1>Our Web Dev Team</h1>
                         <h6>These are the people who keep this site running.</h6>
                     </div>
                 </div>
                 <!-- Peyton Thibodeaux -->
                 <div class="cell-lg-12 bio-area">
                     <div class="range range-sm-middle">
-                        <div class="cell-md-3"><img src="/images/web-team-pics/peyton-thibodeaux.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        <div class="cell-md-3"><img src="/images/bio-pics/peyton-thibodeaux.jpg" alt="" width="360" height="404" class="img-responsive" />
                         </div>
                         <div class="cell-md-6">
                             <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
-                                <h6 class="h6-with-small"><p> Peyton Thibodeaux</p><span class="small text-silver-chalice">Webmaster | Team Member</span></h6>
+                                <h6 id="peyton-thibodeaux" class="h6-with-small"><a href="#peyton-thibodeaux"> Peyton Thibodeaux</a><span class="small text-silver-chalice">Webmaster | Team Member</span></h6>
                                 <p>Peyton is a junior, studying computer science with a minor in mathematics. He's the webmaster for UNT Robotics and in charge of the website that you see in front of you. He enjoys learning and using new technologies and have a passion for creating things. </p>
                                 <ul class="list-inline-lg">
                                     <li>
@@ -57,11 +57,11 @@ head('Our Team', true);
                 <!-- Sebastian King -->
                 <div class="cell-lg-12 bio-area">
                     <div class="range range-sm-middle">
-                        <div class="cell-md-3"><img src="/images/web-team-pics/sebastian-king.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        <div class="cell-md-3"><img src="/images/bio-pics/sebastian-king.jpg" alt="" width="360" height="404" class="img-responsive" />
                         </div>
                         <div class="cell-md-6">
                             <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
-                                <h6 class="h6-with-small"><p> Sebastian King</p><span class="small text-silver-chalice">Alumni | Team Member</span></h6>
+                                <h6 id="sebastian-king" class="h6-with-small"><a href="#sebastian-king"> Sebastian King</a><span class="small text-silver-chalice">Alumni | Team Member</span></h6>
                                 <p>Sebastian is a post-baccalaureate world languages student, with a degree in Computer Science. His role is to oversee the day-to-day running of the organisation and help ensure the organisation best serves the students at UNT. His expertise are programming and electrical engineering and he specialises in networking and remote control systems. He is also responsible for a lot of the more ambitious projects around campus, including the famous Sofabot and our re-usable weather balloon project. </p>
                                 <ul class="list-inline-lg">
                                     <li>
@@ -78,11 +78,11 @@ head('Our Team', true);
                 <!-- Nick Tindle -->
                 <div class="cell-lg-12 bio-area">
                     <div class="range range-sm-middle">
-                        <div class="cell-md-3"><img src="/images/web-team-pics/nick-tindle.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        <div class="cell-md-3"><img src="/images/bio-pics/nick-tindle.jpg" alt="" width="360" height="404" class="img-responsive" />
                         </div>
                         <div class="cell-md-6">
                             <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
-                                <h6 class="h6-with-small"><p> Nicholas Tindle</p><span class="small text-silver-chalice">President | Team Member</span></h6>
+                                <h6 id="nicholas-tindle" class="h6-with-small"><a href="#nicholas-tindle"> Nicholas Tindle</a><span class="small text-silver-chalice">President | Team Member</span></h6>
                                 <p>Nicholas Tindle is a Computer Engineering student at UNT. He works in software engineering and loves hackathons. You can generally find him wearing a hat and probably a sweatshirt. He has a long history of collaboration with UNT Robotics as the first president, a loyal advisor, and now Project Manager. He has also served as an advisor to the Dean and is currently an officer of Engineering United. Nick has helped host numerous events at the university over the years. In his professional life, he works in data analysis, web development, and python scripting. </p>
                                 <ul class="list-inline-lg">
                                     <li>
@@ -99,11 +99,11 @@ head('Our Team', true);
                 <!-- Mason Besmer -->
                 <div class="cell-lg-12 bio-area">
                     <div class="range range-sm-middle">
-                        <div class="cell-md-3"><img src="/images/web-team-pics/mason-besmer.png" alt="" width="360" height="404" class="img-responsive" />
+                        <div class="cell-md-3"><img src="/images/web-team-pics/mason-besmer.jpg" alt="" width="360" height="404" class="img-responsive" />
                         </div>
                         <div class="cell-md-6">
                             <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
-                                <h6 class="h6-with-small"><p> Mason Besmer</p><span class="small text-silver-chalice">Team Member</span></h6>
+                                <h6 id="mason-besmer" class="h6-with-small"><a href="#mason-besmer"> Mason Besmer</a><span class="small text-silver-chalice">Team Member</span></h6>
                                 <p>Mason Besmer is a Computer Science student at UNT. He is a webmaster for UNT Robotics and participates in many student orgs. He creates many things and comes up with ideas for even more. In his free time, he likes to create environments for games and program the website in front of you. He likes organization and loves to tinker with things. Creator of his own magic mirror, Mason is a advocate for building his own electronics. Currently, he is working on a software solution for his Starcube. You can find out more about it on his LinkedIn.</p>
                                 <ul class="list-inline-lg">
                                     <li>
@@ -117,28 +117,27 @@ head('Our Team', true);
                         </div>
                     </div>
                 </div>
-
                 <!-- Template NOTE: images must be 360 by 404 px, and take up less than -->
-<!--                            <div class="cell-lg-12 bio-area">-->
-<!--                                <div class="range range-sm-middle">-->
-<!--                                    <div class="cell-md-3"><img src="/images/web-team-pics/fileName.jpg" alt="" width="360" height="404" class="img-responsive" />-->
-<!--                                    </div>-->
-<!--                                    <div class="cell-md-6">-->
-<!--                                        <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">-->
-<!--                                            <h6 class="h6-with-small"><p> FirstName LastName</p><span class="small text-silver-chalice">Team Member</span></h6>-->
-<!--                                            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>-->
-<!--                                            <ul class="list-inline-lg">-->
-<!--                                                <li>-->
-<!--                                                    <a href="https://www.linkedin.com/in/yourLinkedin" class="icon icon-sm text-primary fa-linkedin"></a>-->
-<!--                                                </li>-->
-<!--                                                <li>-->
-<!--                                                    <a href="https://www.github.com/yourGithub" class="icon icon-sm text-primary fa-github"></a>-->
-<!--                                                </li>-->
-<!--                                            </ul>-->
-<!--                                        </div>-->
-<!--                                    </div>-->
-<!--                                </div>-->
-<!--                            </div>-->
+                <!--            <div class="cell-lg-12 bio-area">-->
+                <!--                <div class="range range-sm-middle">-->
+                <!--                    <div class="cell-md-3"><img src="/images/bio-pics/fileName.jpg" alt="" width="360" height="404" class="img-responsive" />-->
+                <!--                    </div>-->
+                <!--                    <div class="cell-md-6">-->
+                <!--                        <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">-->
+                <!--                            <h6 id="Your-Name" class="h6-with-small"><a href="#Your-Name"> FirstName LastName</a><span class="small text-silver-chalice">Team Member</span></h6>-->
+                <!--                            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>-->
+                <!--                            <ul class="list-inline-lg">-->
+                <!--                                <li>-->
+                <!--                                    <a href="https://www.linkedin.com/in/yourLinkedin" class="icon icon-sm text-primary fa-linkedin"></a>-->
+                <!--                                </li>-->
+                <!--                                <li>-->
+                <!--                                    <a href="https://www.github.com/yourGithub" class="icon icon-sm text-primary fa-github"></a>-->
+                <!--                                </li>-->
+                <!--                            </ul>-->
+                <!--                        </div>-->
+                <!--                    </div>-->
+                <!--                </div>-->
+                <!--            </div>-->
             </div>
         </section>
     </main>
