a98cd7a428084dee79c9df390ad604b6a1858572
aryandamle <aryandamle9@gmail.com>
2021-09-27 23:43:45 -0500

    removed President title and changed profile picture file extension for Aryan

--- about/our-web-team.php
+++ about/our-web-team.php
@@ -10,12 +10,27 @@ head('Our Team', true);
         }
     </style>
     <main class="page-content">
+        <!-- Classic Breadcrumbs-->
+        <section class="breadcrumb-classic">
+            <div class="rd-parallax">
+                <div data-speed="0.25" data-type="media" data-url="/images/breadcrumbs-parallax.jpg" class="rd-parallax-layer"></div>
+                <div data-speed="0" data-type="html" class="rd-parallax-layer section-top-75 section-md-top-150 section-lg-top-260">
+                    <div class="shell">
+                        <ul class="list-breadcrumb">
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/about">About Us</a></li>
+                            <li>Our Web Team</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
         <section class="section-75 section-md-100 section-lg-150">
             <div class="shell range-offset-1">
                 <div class="range">
                     <div class="cell-lg-6">
                         <h1>Our Web Dev Team</h1>
-                        <h6>These people are what keeps this site running.</h6>
+                        <h6>These are the people who keep this site running.</h6>
                     </div>
                 </div>
                 <!-- Peyton Thibodeaux -->
@@ -25,7 +40,7 @@ head('Our Team', true);
                         </div>
                         <div class="cell-md-6">
                             <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
-                                <h6 class="h6-with-small"><a href="#"> Peyton Thibodeaux</a><span class="small text-silver-chalice">Webmaster | Team Member</span></h6>
+                                <h6 id="peyton-thibodeaux" class="h6-with-small"><a href="#peyton-thibodeaux"> Peyton Thibodeaux</a><span class="small text-silver-chalice">Webmaster | Team Member</span></h6>
                                 <p>Peyton is a junior, studying computer science with a minor in mathematics. He's the webmaster for UNT Robotics and in charge of the website that you see in front of you. He enjoys learning and using new technologies and have a passion for creating things. </p>
                                 <ul class="list-inline-lg">
                                     <li>
@@ -46,7 +61,7 @@ head('Our Team', true);
                         </div>
                         <div class="cell-md-6">
                             <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
-                                <h6 class="h6-with-small"><a href="#"> Sebastian King</a><span class="small text-silver-chalice">Alumni | Team Member</span></h6>
+                                <h6 id="sebastian-king" class="h6-with-small"><a href="#sebastian-king"> Sebastian King</a><span class="small text-silver-chalice">Alumni | Team Member</span></h6>
                                 <p>Sebastian is a post-baccalaureate world languages student, with a degree in Computer Science. His role is to oversee the day-to-day running of the organisation and help ensure the organisation best serves the students at UNT. His expertise are programming and electrical engineering and he specialises in networking and remote control systems. He is also responsible for a lot of the more ambitious projects around campus, including the famous Sofabot and our re-usable weather balloon project. </p>
                                 <ul class="list-inline-lg">
                                     <li>
@@ -67,7 +82,7 @@ head('Our Team', true);
                         </div>
                         <div class="cell-md-6">
                             <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
-                                <h6 class="h6-with-small"><a href="#"> Nicholas Tindle</a><span class="small text-silver-chalice">President | Team Member</span></h6>
+                                <h6 id="nicholas-tindle" class="h6-with-small"><a href="#nicholas-tindle"> Nicholas Tindle</a><span class="small text-silver-chalice">President | Team Member</span></h6>
                                 <p>Nicholas Tindle is a Computer Engineering student at UNT. He works in software engineering and loves hackathons. You can generally find him wearing a hat and probably a sweatshirt. He has a long history of collaboration with UNT Robotics as the first president, a loyal advisor, and now Project Manager. He has also served as an advisor to the Dean and is currently an officer of Engineering United. Nick has helped host numerous events at the university over the years. In his professional life, he works in data analysis, web development, and python scripting. </p>
                                 <ul class="list-inline-lg">
                                     <li>
@@ -81,14 +96,35 @@ head('Our Team', true);
                         </div>
                     </div>
                 </div>
-                <!-- Aryan Damle -->
+                <!-- Mason Besmer -->
                 <div class="cell-lg-12 bio-area">
                     <div class="range range-sm-middle">
-                        <div class="cell-md-3"><img src="/images/web-team-pics/aryan-damle.jpeg" alt="" width="360" height="404" class="img-responsive" />
+                        <div class="cell-md-3"><img src="/images/web-team-pics/mason-besmer.jpg" alt="" width="360" height="404" class="img-responsive" />
                         </div>
                         <div class="cell-md-6">
                             <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
-                                <h6 class="h6-with-small"><a href="#"> Aryan Damle</a><span class="small text-silver-chalice">Team Member</span></h6>
+                                <h6 id="mason-besmer" class="h6-with-small"><a href="#mason-besmer"> Mason Besmer</a><span class="small text-silver-chalice">Team Member</span></h6>
+                                <p>Mason Besmer is a Computer Science student at UNT. He is a webmaster for UNT Robotics and participates in many student orgs. He creates many things and comes up with ideas for even more. In his free time, he likes to create environments for games and program the website in front of you. He likes organization and loves to tinker with things. Creator of his own magic mirror, Mason is a advocate for building his own electronics. Currently, he is working on a software solution for his Starcube. You can find out more about it on his LinkedIn.</p>
+                                <ul class="list-inline-lg">
+                                    <li>
+                                        <a href="https://www.linkedin.com/in/masonbesmer" class="icon icon-sm text-primary fa-linkedin"></a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.github.com/shotbyapony" class="icon icon-sm text-primary fa-github"></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Aryan Damle -->
+                <div class="cell-lg-12 bio-area">
+                    <div class="range range-sm-middle">
+                        <div class="cell-md-3"><img src="/images/web-team-pics/aryan-damle.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        </div>
+                        <div class="cell-md-6">
+                            <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
+                                <h6 id="aryan-damle" class="h6-with-small"><a href="#aryan-damle"> Aryan Damle</a><span class="small text-silver-chalice">Team Member</span></h6>
                                 <p>Aryan Damle is a Computer Science student at UNT. He is an aspiring full stack web developer and an avid Home Assistant enthusiast. He mentors a high school robotics team and loves to work on robots in his free time. You can find him at your local car meet on weekends if he isn't busy working on a robot or fixing something in his garage.</p>
                                 <ul class="list-inline-lg">
                                     <li>
@@ -110,7 +146,7 @@ head('Our Team', true);
                 <!--                    </div>-->
                 <!--                    <div class="cell-md-6">-->
                 <!--                        <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">-->
-                <!--                            <h6 class="h6-with-small"><a href="#"> FirstName LastName</a><span class="small text-silver-chalice">Team Member</span></h6>-->
+                <!--                            <h6 id="Your-Name" class="h6-with-small"><a href="#Your-Name"> FirstName LastName</a><span class="small text-silver-chalice">Team Member</span></h6>-->
                 <!--                            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>-->
                 <!--                            <ul class="list-inline-lg">-->
                 <!--                                <li>-->
