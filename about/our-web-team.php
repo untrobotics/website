74f10f6a0f9178d169b7813b4cf010b405c47cf8
Henry <henlegay@gmail.com>
2021-09-29 02:18:10 -0500

    moved files to new folder

--- about/our-web-team.php
+++ about/our-web-team.php
@@ -21,18 +21,25 @@ head('Our Team', true);
                 <!-- Peyton Thibodeaux -->
                 <div class="cell-lg-12 bio-area">
                     <div class="range range-sm-middle">
-                        <div class="cell-md-3"><img src="/images/bio-pics/peyton-thibodeaux.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        <div class="cell-md-3"><img src="/images/bio-pics/peyton-thibodeaux.jpg" alt="" width="360"
+                                                    height="404" class="img-responsive"/>
                         </div>
                         <div class="cell-md-6">
                             <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
-                                <h6 class="h6-with-small"><a href="#"> Peyton Thibodeaux</a><span class="small text-silver-chalice">Webmaster | Team Member</span></h6>
-                                <p>Peyton is a junior, studying computer science with a minor in mathematics. He's the webmaster for UNT Robotics and in charge of the website that you see in front of you. He enjoys learning and using new technologies and have a passion for creating things. </p>
+                                <h6 class="h6-with-small"><a href="#"> Peyton Thibodeaux</a><span
+                                            class="small text-silver-chalice">Webmaster | Team Member</span></h6>
+                                <p>Peyton is a junior, studying computer science with a minor in mathematics. He's the
+                                    webmaster for UNT Robotics and in charge of the website that you see in front of
+                                    you. He enjoys learning and using new technologies and have a passion for creating
+                                    things. </p>
                                 <ul class="list-inline-lg">
                                     <li>
-                                        <a href="https://www.linkedin.com/in/peyton-thibodeaux" class="icon icon-sm text-primary fa-linkedin"></a>
+                                        <a href="https://www.linkedin.com/in/peyton-thibodeaux"
+                                           class="icon icon-sm text-primary fa-linkedin"></a>
                                     </li>
                                     <li>
-                                        <a href="https://www.github.com/peyton232" class="icon icon-sm text-primary fa-github"></a>
+                                        <a href="https://www.github.com/peyton232"
+                                           class="icon icon-sm text-primary fa-github"></a>
                                     </li>
                                 </ul>
                             </div>
@@ -42,18 +49,28 @@ head('Our Team', true);
                 <!-- Sebastian King -->
                 <div class="cell-lg-12 bio-area">
                     <div class="range range-sm-middle">
-                        <div class="cell-md-3"><img src="/images/bio-pics/sebastian-king.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        <div class="cell-md-3"><img src="/images/bio-pics/sebastian-king.jpg" alt="" width="360"
+                                                    height="404" class="img-responsive"/>
                         </div>
                         <div class="cell-md-6">
                             <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
-                                <h6 class="h6-with-small"><a href="#"> Sebastian King</a><span class="small text-silver-chalice">Alumni | Team Member</span></h6>
-                                <p>Sebastian is a post-baccalaureate world languages student, with a degree in Computer Science. His role is to oversee the day-to-day running of the organisation and help ensure the organisation best serves the students at UNT. His expertise are programming and electrical engineering and he specialises in networking and remote control systems. He is also responsible for a lot of the more ambitious projects around campus, including the famous Sofabot and our re-usable weather balloon project. </p>
+                                <h6 class="h6-with-small"><a href="#"> Sebastian King</a><span
+                                            class="small text-silver-chalice">Alumni | Team Member</span></h6>
+                                <p>Sebastian is a post-baccalaureate world languages student, with a degree in Computer
+                                    Science. His role is to oversee the day-to-day running of the organisation and help
+                                    ensure the organisation best serves the students at UNT. His expertise are
+                                    programming and electrical engineering and he specialises in networking and remote
+                                    control systems. He is also responsible for a lot of the more ambitious projects
+                                    around campus, including the famous Sofabot and our re-usable weather balloon
+                                    project. </p>
                                 <ul class="list-inline-lg">
                                     <li>
-                                        <a href="https://www.linkedin.com/in/sebastian-king" class="icon icon-sm text-primary fa-linkedin"></a>
+                                        <a href="https://www.linkedin.com/in/sebastian-king"
+                                           class="icon icon-sm text-primary fa-linkedin"></a>
                                     </li>
                                     <li>
-                                        <a href="https://www.github.com/sebastian-king" class="icon icon-sm text-primary fa-github"></a>
+                                        <a href="https://www.github.com/sebastian-king"
+                                           class="icon icon-sm text-primary fa-github"></a>
                                     </li>
                                 </ul>
                             </div>
@@ -63,18 +80,28 @@ head('Our Team', true);
                 <!-- Nick Tindle -->
                 <div class="cell-lg-12 bio-area">
                     <div class="range range-sm-middle">
-                        <div class="cell-md-3"><img src="/images/bio-pics/nick-tindle.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        <div class="cell-md-3"><img src="/images/bio-pics/nick-tindle.jpg" alt="" width="360"
+                                                    height="404" class="img-responsive"/>
                         </div>
                         <div class="cell-md-6">
                             <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
-                                <h6 class="h6-with-small"><a href="#"> Nicholas Tindle</a><span class="small text-silver-chalice">President | Team Member</span></h6>
-                                <p>Nicholas Tindle is a Computer Engineering student at UNT. He works in software engineering and loves hackathons. You can generally find him wearing a hat and probably a sweatshirt. He has a long history of collaboration with UNT Robotics as the first president, a loyal advisor, and now Project Manager. He has also served as an advisor to the Dean and is currently an officer of Engineering United. Nick has helped host numerous events at the university over the years. In his professional life, he works in data analysis, web development, and python scripting. </p>
+                                <h6 class="h6-with-small"><a href="#"> Nicholas Tindle</a><span
+                                            class="small text-silver-chalice">President | Team Member</span></h6>
+                                <p>Nicholas Tindle is a Computer Engineering student at UNT. He works in software
+                                    engineering and loves hackathons. You can generally find him wearing a hat and
+                                    probably a sweatshirt. He has a long history of collaboration with UNT Robotics as
+                                    the first president, a loyal advisor, and now Project Manager. He has also served as
+                                    an advisor to the Dean and is currently an officer of Engineering United. Nick has
+                                    helped host numerous events at the university over the years. In his professional
+                                    life, he works in data analysis, web development, and python scripting. </p>
                                 <ul class="list-inline-lg">
                                     <li>
-                                        <a href="https://www.linkedin.com/in/ntindle" class="icon icon-sm text-primary fa-linkedin"></a>
+                                        <a href="https://www.linkedin.com/in/ntindle"
+                                           class="icon icon-sm text-primary fa-linkedin"></a>
                                     </li>
                                     <li>
-                                        <a href="https://github.com/ntindle" class="icon icon-sm text-primary fa-github"></a>
+                                        <a href="https://github.com/ntindle"
+                                           class="icon icon-sm text-primary fa-github"></a>
                                     </li>
                                 </ul>
                             </div>
@@ -83,26 +110,31 @@ head('Our Team', true);
                 </div>
 
                 <!--Henry Legay-->
-                            <div class="cell-lg-12 bio-area">
-                                <div class="range range-sm-middle">
-                                    <div class="cell-md-3"><img src="/images/bio-pics/full/henry-legay.jpg" alt="" width="360" height="404" class="img-responsive" />
-                                    </div>
-                                    <div class="cell-md-6">
-                                        <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
-                                            <h6 class="h6-with-small"><a href="#"> Henry Legay</a><span class="small text-silver-chalice">Team Member</span></h6>
-                                            <p> Henry Legay is a Computer Science Student at UNT focused on web development. He is a web developer in Robotics with ready applicable experience and a willingness to learn.</p>
-                                            <ul class="list-inline-lg">q
-                                                <li>
-                                                    <a href="https://www.linkedin.com/in/henrylegay/" class="icon icon-sm text-primary fa-linkedin"></a>
-                                                </li>
-                                                <li>
-                                                    <a href="https://github.com/henlegay" class="icon icon-sm text-primary fa-github"></a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
+                <div class="cell-lg-12 bio-area">
+                    <div class="range range-sm-middle">
+                        <div class="cell-md-3"><img src="/images/web-team-pics/henry-legay.jpg" alt="" width="360"
+                                                    height="404" class="img-responsive"/>
+                        </div>
+                        <div class="cell-md-6">
+                            <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
+                                <h6 class="h6-with-small"> Henry Legay<span
+                                            class="small text-silver-chalice">Team Member</span></h6>
+                                <p> Henry Legay is a Computer Science Student at UNT focused on web development. He is a
+                                    web developer in Robotics with ready applicable experience and a willingness to
+                                    learn.</p>
+                                <ul class="list-inline-lg">
+                                    <li>
+                                        <a href="https://www.linkedin.com/in/henrylegay/"
+                                           class="icon icon-sm text-primary fa-linkedin"></a>
+                                    </li>
+                                    <li>
+                                        <a href="https://github.com/henlegay" class="icon icon-sm text-primary fa-github"></a>
+                                    </li>
+                                </ul>
                             </div>
+                        </div>
+                    </div>
+                </div>
 
                 <!-- Template NOTE: images must be 360 by 404 px, and take up less than -->
                 <!--            <div class="cell-lg-12 bio-area">-->
