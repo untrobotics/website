7b24376396f60b6f92bc677ae6a86ee91eeb333f
mcp31 <mcplana31@gmail.com>
2021-09-28 22:36:26 -0500

    Added myself to the web team page

--- about/our-web-team.php
+++ about/our-web-team.php
@@ -13,8 +13,10 @@ head('Our Team', true);
         <!-- Classic Breadcrumbs-->
         <section class="breadcrumb-classic">
             <div class="rd-parallax">
-                <div data-speed="0.25" data-type="media" data-url="/images/breadcrumbs-parallax.jpg" class="rd-parallax-layer"></div>
-                <div data-speed="0" data-type="html" class="rd-parallax-layer section-top-75 section-md-top-150 section-lg-top-260">
+                <div data-speed="0.25" data-type="media" data-url="/images/breadcrumbs-parallax.jpg"
+                     class="rd-parallax-layer"></div>
+                <div data-speed="0" data-type="html"
+                     class="rd-parallax-layer section-top-75 section-md-top-150 section-lg-top-260">
                     <div class="shell">
                         <ul class="list-breadcrumb">
                             <li><a href="/">Home</a></li>
@@ -36,18 +38,26 @@ head('Our Team', true);
                 <!-- Peyton Thibodeaux -->
                 <div class="cell-lg-12 bio-area">
                     <div class="range range-sm-middle">
-                        <div class="cell-md-3"><img src="/images/bio-pics/peyton-thibodeaux.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        <div class="cell-md-3"><img src="/images/bio-pics/peyton-thibodeaux.jpg" alt="" width="360"
+                                                    height="404" class="img-responsive"/>
                         </div>
                         <div class="cell-md-6">
                             <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
-                                <h6 id="peyton-thibodeaux" class="h6-with-small"><a href="#peyton-thibodeaux"> Peyton Thibodeaux</a><span class="small text-silver-chalice">Webmaster | Team Member</span></h6>
-                                <p>Peyton is a junior, studying computer science with a minor in mathematics. He's the webmaster for UNT Robotics and in charge of the website that you see in front of you. He enjoys learning and using new technologies and have a passion for creating things. </p>
+                                <h6 id="peyton-thibodeaux" class="h6-with-small"><a href="#peyton-thibodeaux"> Peyton
+                                        Thibodeaux</a><span
+                                            class="small text-silver-chalice">Webmaster | Team Member</span></h6>
+                                <p>Peyton is a junior, studying computer science with a minor in mathematics. He's the
+                                    webmaster for UNT Robotics and in charge of the website that you see in front of
+                                    you. He enjoys learning and using new technologies and have a passion for creating
+                                    things. </p>
                                 <ul class="list-inline-lg">
                                     <li>
-                                        <a href="https://www.linkedin.com/in/peyton-thibodeaux" class="icon icon-sm text-primary fa-linkedin"></a>
+                                        <a href="https://www.linkedin.com/in/peyton-thibodeaux"
+                                           class="icon icon-sm text-primary fa-linkedin"></a>
                                     </li>
                                     <li>
-                                        <a href="https://www.github.com/peyton232" class="icon icon-sm text-primary fa-github"></a>
+                                        <a href="https://www.github.com/peyton232"
+                                           class="icon icon-sm text-primary fa-github"></a>
                                     </li>
                                 </ul>
                             </div>
@@ -57,18 +67,28 @@ head('Our Team', true);
                 <!-- Sebastian King -->
                 <div class="cell-lg-12 bio-area">
                     <div class="range range-sm-middle">
-                        <div class="cell-md-3"><img src="/images/bio-pics/sebastian-king.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        <div class="cell-md-3"><img src="/images/bio-pics/sebastian-king.jpg" alt="" width="360"
+                                                    height="404" class="img-responsive"/>
                         </div>
                         <div class="cell-md-6">
                             <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
-                                <h6 id="sebastian-king" class="h6-with-small"><a href="#sebastian-king"> Sebastian King</a><span class="small text-silver-chalice">Alumni | Team Member</span></h6>
-                                <p>Sebastian is a post-baccalaureate world languages student, with a degree in Computer Science. His role is to oversee the day-to-day running of the organisation and help ensure the organisation best serves the students at UNT. His expertise are programming and electrical engineering and he specialises in networking and remote control systems. He is also responsible for a lot of the more ambitious projects around campus, including the famous Sofabot and our re-usable weather balloon project. </p>
+                                <h6 id="sebastian-king" class="h6-with-small"><a href="#sebastian-king"> Sebastian
+                                        King</a><span class="small text-silver-chalice">Alumni | Team Member</span></h6>
+                                <p>Sebastian is a post-baccalaureate world languages student, with a degree in Computer
+                                    Science. His role is to oversee the day-to-day running of the organisation and help
+                                    ensure the organisation best serves the students at UNT. His expertise are
+                                    programming and electrical engineering and he specialises in networking and remote
+                                    control systems. He is also responsible for a lot of the more ambitious projects
+                                    around campus, including the famous Sofabot and our re-usable weather balloon
+                                    project. </p>
                                 <ul class="list-inline-lg">
                                     <li>
-                                        <a href="https://www.linkedin.com/in/sebastian-king" class="icon icon-sm text-primary fa-linkedin"></a>
+                                        <a href="https://www.linkedin.com/in/sebastian-king"
+                                           class="icon icon-sm text-primary fa-linkedin"></a>
                                     </li>
                                     <li>
-                                        <a href="https://www.github.com/sebastian-king" class="icon icon-sm text-primary fa-github"></a>
+                                        <a href="https://www.github.com/sebastian-king"
+                                           class="icon icon-sm text-primary fa-github"></a>
                                     </li>
                                 </ul>
                             </div>
@@ -78,18 +98,29 @@ head('Our Team', true);
                 <!-- Nick Tindle -->
                 <div class="cell-lg-12 bio-area">
                     <div class="range range-sm-middle">
-                        <div class="cell-md-3"><img src="/images/bio-pics/nick-tindle.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        <div class="cell-md-3"><img src="/images/bio-pics/nick-tindle.jpg" alt="" width="360"
+                                                    height="404" class="img-responsive"/>
                         </div>
                         <div class="cell-md-6">
                             <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
-                                <h6 id="nicholas-tindle" class="h6-with-small"><a href="#nicholas-tindle"> Nicholas Tindle</a><span class="small text-silver-chalice">President | Team Member</span></h6>
-                                <p>Nicholas Tindle is a Computer Engineering student at UNT. He works in software engineering and loves hackathons. You can generally find him wearing a hat and probably a sweatshirt. He has a long history of collaboration with UNT Robotics as the first president, a loyal advisor, and now Project Manager. He has also served as an advisor to the Dean and is currently an officer of Engineering United. Nick has helped host numerous events at the university over the years. In his professional life, he works in data analysis, web development, and python scripting. </p>
+                                <h6 id="nicholas-tindle" class="h6-with-small"><a href="#nicholas-tindle"> Nicholas
+                                        Tindle</a><span class="small text-silver-chalice">President | Team Member</span>
+                                </h6>
+                                <p>Nicholas Tindle is a Computer Engineering student at UNT. He works in software
+                                    engineering and loves hackathons. You can generally find him wearing a hat and
+                                    probably a sweatshirt. He has a long history of collaboration with UNT Robotics as
+                                    the first president, a loyal advisor, and now Project Manager. He has also served as
+                                    an advisor to the Dean and is currently an officer of Engineering United. Nick has
+                                    helped host numerous events at the university over the years. In his professional
+                                    life, he works in data analysis, web development, and python scripting. </p>
                                 <ul class="list-inline-lg">
                                     <li>
-                                        <a href="https://www.linkedin.com/in/ntindle" class="icon icon-sm text-primary fa-linkedin"></a>
+                                        <a href="https://www.linkedin.com/in/ntindle"
+                                           class="icon icon-sm text-primary fa-linkedin"></a>
                                     </li>
                                     <li>
-                                        <a href="https://github.com/ntindle" class="icon icon-sm text-primary fa-github"></a>
+                                        <a href="https://github.com/ntindle"
+                                           class="icon icon-sm text-primary fa-github"></a>
                                     </li>
                                 </ul>
                             </div>
@@ -99,18 +130,28 @@ head('Our Team', true);
                 <!-- Mason Besmer -->
                 <div class="cell-lg-12 bio-area">
                     <div class="range range-sm-middle">
-                        <div class="cell-md-3"><img src="/images/web-team-pics/mason-besmer.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        <div class="cell-md-3"><img src="/images/web-team-pics/mason-besmer.jpg" alt="" width="360"
+                                                    height="404" class="img-responsive"/>
                         </div>
                         <div class="cell-md-6">
                             <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
-                                <h6 id="mason-besmer" class="h6-with-small"><a href="#mason-besmer"> Mason Besmer</a><span class="small text-silver-chalice">Team Member</span></h6>
-                                <p>Mason Besmer is a Computer Science student at UNT. He is a webmaster for UNT Robotics and participates in many student orgs. He creates many things and comes up with ideas for even more. In his free time, he likes to create environments for games and program the website in front of you. He likes organization and loves to tinker with things. Creator of his own magic mirror, Mason is a advocate for building his own electronics. Currently, he is working on a software solution for his Starcube. You can find out more about it on his LinkedIn.</p>
+                                <h6 id="mason-besmer" class="h6-with-small"><a href="#mason-besmer"> Mason
+                                        Besmer</a><span class="small text-silver-chalice">Team Member</span></h6>
+                                <p>Mason Besmer is a Computer Science student at UNT. He is a webmaster for UNT Robotics
+                                    and participates in many student orgs. He creates many things and comes up with
+                                    ideas for even more. In his free time, he likes to create environments for games and
+                                    program the website in front of you. He likes organization and loves to tinker with
+                                    things. Creator of his own magic mirror, Mason is a advocate for building his own
+                                    electronics. Currently, he is working on a software solution for his Starcube. You
+                                    can find out more about it on his LinkedIn.</p>
                                 <ul class="list-inline-lg">
                                     <li>
-                                        <a href="https://www.linkedin.com/in/masonbesmer" class="icon icon-sm text-primary fa-linkedin"></a>
+                                        <a href="https://www.linkedin.com/in/masonbesmer"
+                                           class="icon icon-sm text-primary fa-linkedin"></a>
                                     </li>
                                     <li>
-                                        <a href="https://www.github.com/shotbyapony" class="icon icon-sm text-primary fa-github"></a>
+                                        <a href="https://www.github.com/shotbyapony"
+                                           class="icon icon-sm text-primary fa-github"></a>
                                     </li>
                                 </ul>
                             </div>
@@ -120,18 +161,53 @@ head('Our Team', true);
                 <!-- Aryan Damle -->
                 <div class="cell-lg-12 bio-area">
                     <div class="range range-sm-middle">
-                        <div class="cell-md-3"><img src="/images/web-team-pics/aryan-damle.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        <div class="cell-md-3"><img src="/images/web-team-pics/aryan-damle.jpg" alt="" width="360"
+                                                    height="404" class="img-responsive"/>
                         </div>
                         <div class="cell-md-6">
                             <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
-                                <h6 id="aryan-damle" class="h6-with-small"><a href="#aryan-damle"> Aryan Damle</a><span class="small text-silver-chalice">Team Member</span></h6>
-                                <p>Aryan Damle is a Computer Science student at UNT. He is an aspiring full stack web developer and an avid Home Assistant enthusiast. He mentors a high school robotics team and loves to work on robots in his free time. You can find him at your local car meet on weekends if he isn't busy working on a robot or fixing something in his garage.</p>
+                                <h6 id="aryan-damle" class="h6-with-small"><a href="#aryan-damle"> Aryan Damle</a><span
+                                            class="small text-silver-chalice">Team Member</span></h6>
+                                <p>Aryan Damle is a Computer Science student at UNT. He is an aspiring full stack web
+                                    developer and an avid Home Assistant enthusiast. He mentors a high school robotics
+                                    team and loves to work on robots in his free time. You can find him at your local
+                                    car meet on weekends if he isn't busy working on a robot or fixing something in his
+                                    garage.</p>
                                 <ul class="list-inline-lg">
                                     <li>
-                                        <a href="https://www.linkedin.com/in/aryan-damle-8691b11bb/" class="icon icon-sm text-primary fa-linkedin"></a>
+                                        <a href="https://www.linkedin.com/in/aryan-damle-8691b11bb/"
+                                           class="icon icon-sm text-primary fa-linkedin"></a>
                                     </li>
                                     <li>
-                                        <a href="https://github.com/aryan-damle" class="icon icon-sm text-primary fa-github"></a>
+                                        <a href="https://github.com/aryan-damle"
+                                           class="icon icon-sm text-primary fa-github"></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Mary Plana -->
+                <div class="cell-lg-12 bio-area">
+                    <div class="range range-sm-middle">
+                        <div class="cell-md-3"><img src="/images/bio-pics/mary-plana.jpg" alt="" width="360" height="404"
+                                                    class="img-responsive"/>
+                        </div>
+                        <div class="cell-md-6">
+                            <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
+                                <h6 id="Your-Name" class="h6-with-small"><a href="#Your-Name"> Mary
+                                        Plana</a><span class="small text-silver-chalice">Team Member</span></h6>
+                                <p> Mary Plana is a Computer Science Student at UNT with studies focused on Front End Development. She loves designing and implementing the user interface of a project. She has a natural curiosity about the world
+                                    and loves to learn and improve her skills. She is currently the president of Application Development Organization. She facilitates the meeting and leads student UI
+                                    designers to design, implement, and improve the user interface of projects.</p>
+                                <ul class="list-inline-lg">
+                                    <li>
+                                        <a href="https://www.linkedin.com/in/mary-plana/"
+                                           class="icon icon-sm text-primary fa-linkedin"></a>
+                                    </li>
+                                    <li>
+                                        <a href="https://github.com/mcp31"
+                                           class="icon icon-sm text-primary fa-github"></a>
                                     </li>
                                 </ul>
                             </div>
