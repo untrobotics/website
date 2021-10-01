78158700b13b5108e46424a241b4b5ab7f714363
samin2668 <44666180+samin2668@users.noreply.github.com>
2021-09-30 21:28:35 -0500

    URW-130
    * Added Samin Yasar to web team page
    
    * locased id names

--- about/our-web-team.php
+++ about/our-web-team.php
@@ -161,7 +161,27 @@ head('Our Team', true);
                         </div>
                     </div>
                 </div>
-
+                <!-- Samin Yasar -->
+                <div class="cell-lg-12 bio-area">
+                    <div class="range range-sm-middle">
+                        <div class="cell-md-3"><img src="/images/web-team-pics/samin-yasar.jpg" alt="" width="360" height="404" class="img-responsive" />
+                        </div>
+                        <div class="cell-md-6">
+                            <div class="inset-xl-right-70 inset-xl-left-70 inset-left-15 inset-right-15">
+                                <h6 id="samin-yasar" class="h6-with-small"><a href="#samin-yasar"> Samin Yasar</a><span class="small text-silver-chalice">Team Member</span></h6>
+                                <p>Samin Yasar is a senior Computer Science student at UNT. He is a team member of UNT Robotics webmaster helping maintain UNT Robotics website. He is also a part of the Application Development Organization as a team member. He likes to learn new things and solve complex problems.</p>
+                                <ul class="list-inline-lg">
+                                    <li>
+                                        <a href="https://www.linkedin.com/in/samin2668" class="icon icon-sm text-primary fa-linkedin"></a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.github.com/samin2668" class="icon icon-sm text-primary fa-github"></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <!-- Template NOTE: images must be 360 by 404 px, and take up less than -->
                 <!--            <div class="cell-lg-12 bio-area">-->
                 <!--                <div class="range range-sm-middle">-->
