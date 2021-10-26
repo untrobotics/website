37d63eaf1fb9bc2d9eb5cd05b1457c0a9379e577
Henry <henlegay@gmail.com>
2021-10-26 00:21:09 -0500

    Created the visual foundation for the My Profile page

--- me/index.php
+++ me/index.php
@@ -5,84 +5,178 @@ head('My Profile', true, true);
 ?>
 <head>
     <style>
-        .ellipse{
-            margin: 97px auto 40px auto;
-            height: 294px;
-            width: 294px;
-            left: -1919px;
-            top: -150px;
-            border-radius: 50%;
-            background: #C4C4C4;
-        }
-        .rectangle{
-            position: absolute;
-            width: 941px;
-            height: 166px;
-
-            border: 1px solid #B7B7B7;
-            box-sizing: border-box;
-            border-radius: 30px;
-        }
-        .email{
-            /*position: absolute;*/
-            width: 70px;
-            height: 28px;
-
-            /*font-family: Ubuntu;*/
+        .profileName{
+            text-align: center;
+            font-family: Ubuntu;
             font-style: normal;
             font-weight: bold;
+            font-size: 48px;
+            line-height: 55px;
+            color: #18AC5C;
+        }
+        .profileText{
+            text-align: center;
+            font-family: Ubuntu;
+            font-style: normal;
+            font-weight: normal;
             font-size: 24px;
             line-height: 28px;
-            /* identical to box height */
             color: #000000;
         }
-        .editButton{
-            position: absolute;
-            width: 40px;
-            height: 25px;
-
-            /*font-family: Ubuntu;*/
+        .center {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 50%;
+        }
+        .button{
+            display: inline-block;
+            background: #007A38;
+            border-radius: 6px;
+            padding: 5px;
+            text-align: center;
+            font-family: Ubuntu;
             font-style: normal;
             font-weight: normal;
             font-size: 22px;
-            line-height: 25px;
-
-            color: #000000;
-            
+            color: #FFFFFF;
         }
-
+        .orangeBox{
+            background: #EDEDED;
+            border-radius: 30px;
+        }
     </style>
 </head>
-
+<section>
 <main class="page-content">
-    <!-- Classic Breadcrumbs-->
-    <section class="section-50">
-        <div class="shell text-sm-left">
-            <div class="blog-post">
-                <div class="range">
-                    <div class="cell-md-preffix-2 cell-lg-9 cell-md-8 cell-xl-8">
-                        <h1 style="text-align:center">My Profile</h1>
-                        <div class="ellipse"></div>
-                        <h3 style="text-align:center; color: #18AC5C; "><u>Jonathan Doe</u></h3>
-                        <pre>Profile Info</pre>
-                        <div class="email">Email</div>
-                        <div class="editButton">Edit Button</div>
-<!--                        <div class="rectangle"></div>-->
-<!--                        <p>Profile info.</p>-->
+    <div class="row">
+        <div class="col-md-2 col-xs-0"></div>
+        <div class="col-md-8">
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+                    <h2 style = "text-align: center">MY PROFILE</h2></div>
+                <div class="col-md-3"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4"><img width = 300 class = "center" src="/images/bio-pics/temp-pic.jpg"></div>
+                <div class="col-md-4"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4">
+                    <h3 class="profileName"><?php echo $userinfo['name']; ?></h3>
+                </div>
+                <div class="col-md-4"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-8"><textarea rows = "4" style = "width: 100%"></textarea></div>
+                <div class="col-md-2"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <h4 class = "profileText" style = "text-align:right">Email:</h4>
+                </div>
+                <div class="col-md-4">
+                    <h3 class = "profileText"><?php echo $userinfo['email']; ?></h3>
+                </div>
+                <div class="col-md-4"><h3 class = "button">Edit</h3></div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <h4 class = "profileText" style = "text-align:right">Password:</h4>
+                </div>
+                <div class="col-md-4">
+                    <h3 class = "profileText">**********</h3>
+                </div>
+                <div class="col-md-4"><h3 class = "button">Edit</h3></div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <h4 class = "profileText" style = "text-align:right">Phone Number:</h4>
+                </div>
+                <div class="col-md-4">
+                    <h3 class = "profileText"><?php echo $userinfo['phone']; ?></h3>
+                </div>
+                <div class="col-md-4"><h3 class = "button">Edit</h3></div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <h4 class = "profileText" style = "text-align:right">Discord Account:</h4>
+                </div>
+                <div class="col-md-4">
+                    <h3 class = "profileText"><?php echo $userinfo['discord']; ?></h3>
+                </div>
+                <div class="col-md-4"><h3 class = "button">Edit</h3></div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <h4 class = "profileText" style = "text-align:right">LinkedIn Account:</h4>
+                </div>
+                <div class="col-md-4">
+                    <h3 class = "profileText"><?php echo $userinfo['linkedin']; ?></h3>
+                </div>
+                <div class="col-md-4"><h3 class = "button">Edit</h3></div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 orangeBox">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <h4 class = "profileText" style = "text-align:right">UNT EUID:</h4>
+                        </div>
+                        <div class="col-md-4">
+                            <h4 class = "profileText" style = "text-align:right"><?php echo $userinfo['euid']; ?></h4>
+                        </div>
+                        <div class="col-md-4">
+                            <h3 class = "button">Edit</h3>
+                        </div>
                     </div>
-                    <div class="cell-xs-12">
-                        <div class="left-aside"><span class="small text-darker text-uppercase text-bold text-spacing-340"><?php echo $userinfo['name']; ?></span>
-                            <div class="divider-custom veil reveal-md-block"></div>
-                            <ul class="list text-md-center">
-                                <li><a href="#" class="ioon icon-sm icon-darker fa-check"></a></li>
-                                <li><a href="#" class="ioon icon-sm icon-darker fa-times"></a></li>
-                            </ul>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <h4 class = "profileText" style = "text-align:right">Graduation Date:</h4>
+                        </div>
+                        <div class="col-md-4">
+                            <h4 class = "profileText" style = "text-align:center">Spring 2023</h4>
+                        </div>
+                        <div class="col-md-4">
+                            <h3 class = "button">Edit</h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12"><br></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h4 class = "profileText" style = "text-align:right">CampusLabs Member</h4>
+                        </div>
+                        <div class="col-md-3">
+                            <input type="checkbox"  name="campusLabsBox" value="campusLabs Member">
+                        </div>
+                        <div class="col-md-3">
+                            <h4 class = "profileText" style = "text-align:right">Good Standing</h4>
+                        </div>
+                        <div class="col-md-3">
+                            <input type="checkbox"  name="campusLabsBox" value="Good Standing">
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <h3 class = "button">Order History</h3>
+                </div>
+                <div class="col-md-4">
+                    <h3 class = "button">Payment</h3>
+                </div>
+                <div class="col-md-4">
+                    <h3 class = "button">Dues</h3>
+                </div>
+            </div>
         </div>
-    </section>
+        <div class="col-md-2 col-xs-0"></div>
+    </div>
 </main>
 
 <?php
