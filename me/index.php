61e5e6a4031fb24b6445bcb77049dffe7ad2e76c
Henry <henlegay@gmail.com>
2021-11-20 01:29:58 -0600

    Centered final bottom three button divs

--- me/index.php
+++ me/index.php
@@ -6,6 +6,7 @@ head('My Profile', true, true);
 <head>
     <style>
         .profileName{
+            padding: 20px 20px 20px 20px;
             text-align: center;
             font-family: Ubuntu;
             font-style: normal;
@@ -33,7 +34,23 @@ head('My Profile', true, true);
             display: inline-block;
             background: #007A38;
             border-radius: 6px;
-            padding: 5px;
+            margin-bottom: 25px;
+            padding: 2px 15px 2px 15px;
+            text-align: center;
+            font-family: Ubuntu;
+            font-style: normal;
+            font-weight: normal;
+            font-size: 22px;
+            color: #FFFFFF;
+        }
+        .bottomButton{
+            position: relative;
+            width: 250px;
+            display: inline-block;
+            background: #007A38;
+            border-radius: 6px;
+            margin-bottom: 25px;
+            padding: 3px 15px 3px 15px;
             text-align: center;
             font-family: Ubuntu;
             font-style: normal;
@@ -42,9 +59,14 @@ head('My Profile', true, true);
             color: #FFFFFF;
         }
         .orangeBox{
+            padding-top: 20px;
+            padding-bottom: 20px;
             background: #EDEDED;
             border-radius: 30px;
+            margin-bottom: 3%;
         }
+
+
     </style>
 </head>
 <section>
@@ -54,25 +76,25 @@ head('My Profile', true, true);
         <div class="col-md-8">
             <div class="row">
                 <div class="col-md-3"></div>
-                <div class="col-md-6">
-                    <h2 style = "text-align: center">MY PROFILE</h2></div>
+                <div class="col-md-6" style = "padding-top: 20px">
+                    <h2 style = "text-align: center;">MY PROFILE</h2></div>
                 <div class="col-md-3"></div>
             </div>
             <div class="row">
                 <div class="col-md-4"></div>
-                <div class="col-md-4"><img width = 300 class = "center" src="/images/bio-pics/temp-pic.jpg"></div>
+                <div class="col-md-4"><img width = 300 class = "center" style = "padding:5% 5% 5% 5%;" src="/images/bio-pics/temp-pic.jpg"></div>
                 <div class="col-md-4"></div>
             </div>
             <div class="row">
                 <div class="col-md-4"></div>
-                <div class="col-md-4">
+                <div class="col-md-4" style = "">
                     <h3 class="profileName"><?php echo $userinfo['name']; ?></h3>
                 </div>
                 <div class="col-md-4"></div>
             </div>
             <div class="row">
                 <div class="col-md-2"></div>
-                <div class="col-md-8"><textarea rows = "4" style = "width: 100%"></textarea></div>
+                <div class="col-md-8"><textarea rows = "4" style = "width: 100%; margin-bottom: 5%;"></textarea></div>
                 <div class="col-md-2"></div>
             </div>
             <div class="row">
@@ -127,7 +149,7 @@ head('My Profile', true, true);
                             <h4 class = "profileText" style = "text-align:right">UNT EUID:</h4>
                         </div>
                         <div class="col-md-4">
-                            <h4 class = "profileText" style = "text-align:right"><?php echo $userinfo['euid']; ?></h4>
+                            <h4 class = "profileText" style = "text-align:center">1234567</h4>
                         </div>
                         <div class="col-md-4">
                             <h3 class = "button">Edit</h3>
@@ -148,10 +170,10 @@ head('My Profile', true, true);
                         <div class="col-md-12"><br></div>
                     </div>
                     <div class="row">
-                        <div class="col-md-3">
-                            <h4 class = "profileText" style = "text-align:right">CampusLabs Member</h4>
+                        <div class="col-md-3" style = "left:14%">
+                            <h4 class = "profileText">CampusLabs Member</h4>
                         </div>
-                        <div class="col-md-3">
+                        <div class="col-md-3" style = "left:13%">
                             <input type="checkbox"  name="campusLabsBox" value="campusLabs Member">
                         </div>
                         <div class="col-md-3">
@@ -163,18 +185,18 @@ head('My Profile', true, true);
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <h3 class = "button">Order History</h3>
-                </div>
-                <div class="col-md-4">
-                    <h3 class = "button">Payment</h3>
-                </div>
-                <div class="col-md-4">
-                    <h3 class = "button">Dues</h3>
+                <div class="row">
+                    <div class="col-md-4">
+                        <h3 class = "bottomButton" style ="float: right">Order History</h3>
+                    </div>
+                    <div class="col-md-4" style = "margin: 0 auto">
+                        <h3 class = "bottomButton" style = "display:block; margin: 0 auto">Payment</h3>
+                    </div>
+                    <div class="col-md-4">
+                        <h3 class = "bottomButton">Dues</h3>
+                    </div>
                 </div>
             </div>
-        </div>
         <div class="col-md-2 col-xs-0"></div>
     </div>
 </main>
