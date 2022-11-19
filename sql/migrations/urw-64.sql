73ab4591836d7e113fa9e798174751dd2c3d73f6
Kenneth Chen <74205780+Chenneth@users.noreply.github.com>
2022-11-18 20:59:53 -0600

    Reset files to develop branch version

--- sql/migrations/urw-64.sql
+++ sql/migrations/urw-64.sql
@@ -1,13 +1,1 @@
-ALTER TABLE ftpusers
-ADD UNIQUE (name);
-
-ALTER TABLE ftpusers
-MODIFY name char(128) NOT NULL;
-
-ALTER TABLE ftpusers
-MODIFY passwd char(128) NOT NULL;
-
-
-
-
-
+CREATE TABLE ftpinvites (id int primary key auto_increment, email varchar(255), registration_token varchar(16));
