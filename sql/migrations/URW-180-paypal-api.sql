896d8a64de4aa39755d0f3cc781d985b3eef123a
kenneth-w-chen <74205780+Kenneth-W-Chen@users.noreply.github.com>
2024-09-21 09:34:43 -0500

    Fix dues shirt external IDs

--- sql/migrations/URW-180-paypal-api.sql
+++ sql/migrations/URW-180-paypal-api.sql
@@ -4,29 +4,30 @@ VALUES ('dues', 10.00, 0.00, 0.00, 'Dues', '1 semester');
 INSERT INTO paypal_items(item_type, sales_price, cost, tax, item_name, variant_name)
 VALUES ('dues', 20.00, 0.00, 0.00, 'Dues', '2 semester');
 
-INSERT INTO paypal_items(item_type, sales_price, cost, tax, item_name, discount, discount_required_item, external_id, variant_name)
-VALUES ('printful_product', 25.00, 0.00, 0.00, 'Dues Shirt', 10.00, 'dues', '632b8e41a865f1', 'XS');
+-- official name is "UNT Robotics Standard Embroidered T-Shirt"
+INSERT INTO paypal_items(item_type, sales_price, cost, tax, item_name, discount, discount_required_item, external_id, variant_name, item_category)
+VALUES ('printful_product', 25.00, 0.00, 0.00, 'Dues Shirt', 10.00, 'dues', '3508512951', 'XS', 'Shirt');
 
-INSERT INTO paypal_items(item_type, sales_price, cost, tax, item_name, discount, discount_required_item, external_id, variant_name)
-VALUES ('printful_product', 25.00, 0.00, 0.00, 'Dues Shirt', 10.00, 'dues', '632b8e41a86664', 'S');
+INSERT INTO paypal_items(item_type, sales_price, cost, tax, item_name, discount, discount_required_item, external_id, variant_name, item_category)
+VALUES ('printful_product', 25.00, 0.00, 0.00, 'Dues Shirt', 10.00, 'dues', '3508512952', 'S', 'Shirt');
 
-INSERT INTO paypal_items(item_type, sales_price, cost, tax, item_name, discount, discount_required_item, external_id, variant_name)
-VALUES ('printful_product', 25.00, 0.00, 0.00, 'Dues Shirt', 10.00, 'dues', '632b8e41a866a1', 'M');
+INSERT INTO paypal_items(item_type, sales_price, cost, tax, item_name, discount, discount_required_item, external_id, variant_name, item_category)
+VALUES ('printful_product', 25.00, 0.00, 0.00, 'Dues Shirt', 10.00, 'dues', '3508512953', 'M', 'Shirt');
 
-INSERT INTO paypal_items(item_type, sales_price, cost, tax, item_name, discount, discount_required_item, external_id, variant_name)
-VALUES ('printful_product', 25.00, 0.00, 0.00, 'Dues Shirt', 10.00, 'dues', '632b8e41a866e2', 'L');
+INSERT INTO paypal_items(item_type, sales_price, cost, tax, item_name, discount, discount_required_item, external_id, variant_name, item_category)
+VALUES ('printful_product', 25.00, 0.00, 0.00, 'Dues Shirt', 10.00, 'dues', '3508512954', 'L', 'Shirt');
 
-INSERT INTO paypal_items(item_type, sales_price, cost, tax, item_name, discount, discount_required_item, external_id, variant_name)
-VALUES ('printful_product', 25.00, 0.00, 0.00, 'Dues Shirt', 10.00, 'dues', '632b8e41a86724', 'XL');
+INSERT INTO paypal_items(item_type, sales_price, cost, tax, item_name, discount, discount_required_item, external_id, variant_name, item_category)
+VALUES ('printful_product', 25.00, 0.00, 0.00, 'Dues Shirt', 10.00, 'dues', '3508512955', 'XL', 'Shirt');
 
-INSERT INTO paypal_items(item_type, sales_price, cost, tax, item_name, discount, discount_required_item, external_id, variant_name)
-VALUES ('printful_product', 25.00, 0.00, 0.00, 'Dues Shirt', 10.00, 'dues', '632b8e41a86761', '2XL');
+INSERT INTO paypal_items(item_type, sales_price, cost, tax, item_name, discount, discount_required_item, external_id, variant_name, item_category)
+VALUES ('printful_product', 25.00, 0.00, 0.00, 'Dues Shirt', 10.00, 'dues', '3508512957', '2XL', 'Shirt');
 
-INSERT INTO paypal_items(item_type, sales_price, cost, tax, item_name, discount, discount_required_item, external_id, variant_name)
-VALUES ('printful_product', 25.00, 0.00, 0.00, 'Dues Shirt', 10.00, 'dues', '632b8e41a867a9', '3XL');
+INSERT INTO paypal_items(item_type, sales_price, cost, tax, item_name, discount, discount_required_item, external_id, variant_name, item_category)
+VALUES ('printful_product', 25.00, 0.00, 0.00, 'Dues Shirt', 10.00, 'dues', '3508512962', '3XL', 'Shirt');
 
-INSERT INTO paypal_items(item_type, sales_price, cost, tax, item_name, discount, discount_required_item, external_id, variant_name)
-VALUES ('printful_product', 25.00, 0.00, 0.00, 'Dues Shirt', 10.00, 'dues', '632b8e41a867e6', '4XL');
+INSERT INTO paypal_items(item_type, sales_price, cost, tax, item_name, discount, discount_required_item, external_id, variant_name, item_category)
+VALUES ('printful_product', 25.00, 0.00, 0.00, 'Dues Shirt', 10.00, 'dues', '3508512963', '4XL', 'Shirt');
 
 INSERT INTO paypal_items_config(ttl, config_name)
 VALUES(86400, 'printful_product');
